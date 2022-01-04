d8a0c7302939dbb28379caea5948be3ded6cb779
Hain Thit <hainlthit@gmail.com>
2022-01-03 20:51:26 -0500

    basic JS functionality

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 console.log("hello")
+
+fetchData()
+
+function fetchData() {
+    fetch("http://localhost:3000/ant00")
+    .then(resp => resp.json())
+    .then(data => renderVillager(data))
+}
+
+function renderVillager(villager) {
+    const villagerMenu = document.getElementById('villager-menu')
+    const villagerImage = document.createElement('img')
+    villagerImage.src = villager.icon_uri
+
+    const villagerName = document.createElement('h1')
+    villagerName.innerHTML = villager.name
+
+    villagerMenu.appendChild(villagerImage)
+    villagerMenu.appendChild(villagerName)
+    // consider mouse-over event to show names
+
+    villagerImage.addEventListener('click', (event) => {
+        
+        const detVillImg = document.querySelector('.villager-image')
+        detVillImg.src = villager.image_uri
+    })
+    
+}
