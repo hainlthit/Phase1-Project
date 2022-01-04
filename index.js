ffeafdaaee612314a596061f853ae57f026e0b9a
Jennifer Tchai <jentchai@gmail.com>
2022-01-04 18:49:23 -0500

    dislike button

--- index.js
+++ index.js
@@ -17,13 +17,65 @@ function renderVillager(villager) {
     villagerName.innerHTML = villager.name
 
     villagerMenu.appendChild(villagerImage)
-    villagerMenu.appendChild(villagerName)
+    //villagerMenu.appendChild(villagerName)
     // consider mouse-over event to show names
 
     villagerImage.addEventListener('click', (event) => {
+
+        event.preventDefault();
         
         const detVillImg = document.querySelector('.villager-image')
         detVillImg.src = villager.image_uri
+        
+        const detVillName = document.querySelector('.name')
+        detVillName.innerText = villager.name
+
+        const detVillGender = document.querySelector('.gender')
+        detVillGender.innerText = villager.gender
+
+        const detVillSpecies = document.querySelector('.species')
+        detVillSpecies.innerText = villager.species
+
+        const detVillPersonality = document.querySelector('.personality')
+        detVillPersonality.innerText = villager.personality
+
+        const detVillBirthday = document.querySelector('.birthday')
+        detVillBirthday.innerText = villager.birthday
+
+        const detVillLikes = document.querySelector('.likes')
+        detVillLikes.innerText = villager.likes
+
+
+        const likeBtn = document.querySelector('.likeBtn')
+        likeBtn.innerText = "Like ❤️"
+        document.getElementById("villager-detail").appendChild(likeBtn)
+        likeBtn.addEventListener("click", (event) => {
+            event.preventDefault();
+                console.log(villager.likes)
+            const oldLikes = document.querySelector('.likes')
+
+            const newLikes = +oldLikes.innerText + 1
+            oldLikes.innerText = newLikes
+        })
+
+        const dislikeBtn = document.querySelector('.dislikeBtn')
+        dislikeBtn.innerText = "Dislike ❤️"
+        document.getElementById("villager-detail").appendChild(dislikeBtn)
+        dislikeBtn.addEventListener("click", (event) => {
+                event.preventDefault();
+                console.log(villager.likes)
+            const oldLikes = document.querySelector('.likes')
+
+            const newLikes = +oldLikes.innerText - 1
+            oldLikes.innerText = newLikes
+
+        })
+
     })
-    
+
 }
+
+function renderComments(villagers) {
+    console.log(villagers.comments)
+}
+renderComments()
