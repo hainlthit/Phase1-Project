d6256ab73bb1459d1c491128bb328b3aeae491b0
Jennifer Tchai <jentchai@gmail.com>
2022-01-05 21:23:26 -0500

    added CSS color

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ likeBtn.addEventListener("click", (event) => {
 })
 
 const dislikeBtn = document.querySelector('.dislikeBtn')
-dislikeBtn.innerText = "Dislike ❤️"
+dislikeBtn.innerText = "Dislike ✘"
 document.getElementById("villager-detail").appendChild(dislikeBtn)
 dislikeBtn.addEventListener("click", (event) => {
     const oldLikes = document.querySelector('.likes')
